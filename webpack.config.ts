a16ca8c89b0c4020df12cc3ce60fbc0da01861ed
Сергей Римша <Sergey.Rimsha.ru@gmail.com>
2023-12-06 16:56:19 +0300

    #9 add prod build plugin 'MiniCssExtractPlugin'

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 type Mode = 'production' | 'development'
@@ -12,6 +14,7 @@ interface EnvVariables {
 
 export default (env: EnvVariables) => {
     const isDev = env.mode === 'development';
+    const isProd = env.mode === 'production';
 
     const config: webpack.Configuration = {
         mode: env.mode ?? 'development',
@@ -23,7 +26,11 @@ export default (env: EnvVariables) => {
         },
         plugins: [
             new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
-            new webpack.ProgressPlugin(),
+            isDev && new webpack.ProgressPlugin(),
+            isProd && new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css'
+            }),
         ],
         module: {
             rules: [
@@ -31,7 +38,7 @@ export default (env: EnvVariables) => {
                     test: /\.s[ac]ss$/i,
                     use: [
                         // Creates `style` nodes from JS strings
-                        "style-loader",
+                        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
                         // Translates CSS into CommonJS
                         "css-loader",
                         // Compiles Sass to CSS
